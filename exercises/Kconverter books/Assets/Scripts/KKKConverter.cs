9333669e1343512defebc95bef201b2f62efcd53
JohnRay <raschehan@gmail.com>
2018-09-10 20:18:02 +0300

    kkk

---
+++ exercises/Kconverter books/Assets/Scripts/KKKConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace sech
+{
+    public static class KKKConverter
+    {
+        //public KKKConverter()
+        //{
+
+        //}
+
+        public static String Convert(this int var)
+        {
+            int c = 1;
+            double d = var / Math.Pow(1000, c);
+            if (d < 1000)
+            {
+                c = 0;
+                d = var;
+            }
+            while (d > 1000)
+            {
+                d = 0;
+                c++;
+                d = var / Math.Pow(1000, c);
+            }
+
+            string k = "";
+            for (int j = 0; j < c; j++)
+                k += "K";
+
+            return String.Format("{0:.##}", d) + " " + k;
+        }
+    }
+}
---
+++ exercises/Kconverter books/Assets/Scripts/TextChanged.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using sech;
+
+public class TextChanged : MonoBehaviour
+{
+
+    //KKKConverter k = new KKKConverter();
+
+    [SerializeField] private Text myText;
+    [SerializeField] private Text label;
+    [SerializeField] private InputField inputField;
+
+    public string outStr;
+
+    private void Start()
+    {
+        InputField input = GetComponent<InputField>();
+        if (input != null)
+            input.onValueChange.AddListener(SetText);
+    }
+
+    public void SetText(string arg0)
+    {
+        Debug.Log(arg0);
+        int i = -1;
+        int.TryParse(arg0, out i);
+        label.text = i.Convert();//k.Convert(i);
+    }
+
+}
