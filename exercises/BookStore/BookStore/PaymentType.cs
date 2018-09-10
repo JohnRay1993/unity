1984a4a79abffa31122d3e86850357f0cf647fe5
JohnRay <raschehan@gmail.com>
2018-09-10 17:18:04 +0300

    1
    1

---
+++ exercises/BookStore/BookStore/PaymentType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public enum PaymentType
+    {
+        none,
+        cash,
+        card,
+        installments,
+        credit,
+        comeback,
+        discount
+    };
+}
---
+++ exercises/BookStore/BookStore/ProductType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    public enum ProductType
+    {
+        none,
+        book,
+        magazine,
+        newspaper,
+        notepad,
+        notebook,
+        picture,
+        calendar
+    };
+}
