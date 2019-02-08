77ba67977c4cb9a560160a7d682c30d606495e1f
JohnRay <raschehan@gmail.com>
2019-02-08 17:21:18 +0300

    08022019

--- exercises/light/Assets/scripts/plane_creator.cs
+++ exercises/light/Assets/scripts/plane_creator.cs
@@ -6,39 +6,96 @@ public class plane_creator : MonoBehaviour
 {
     public GameObject go;
 
-    private Transform previous_object_transform;
+    private Transform[,] previous_object_transform;
+    private int x = 0, y = 0, z = 0;
+    float z_angle;
+    float x_len ;
+    float y_pos;
+    float last_y_pos = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (go != null)
+        previous_object_transform = new Transform[100, 100];
+        //if (go != null)
+        //{
+        //    for (int x = 0; x < 10; x++)
+        //    {
+        //        double z_angle = Random.Range(-30, 30);
+        //        go.transform.rotation = Quaternion.Euler(0, 0, (float)z_angle);
+        //        double x_len = 1 / Mathf.Cos((float)z_angle);
+        //        double y_pos = (x_len * Mathf.Sin((float)z_angle)) / 0.2;
+        //        go.transform.position = new Vector3(x * 10, (float)y_pos, 0);
+        //        go.transform.localScale = new Vector3((float)x_len, 1, 1);
+        //        //new Quaternion(0, 0, Random.Range(-1, 1), 1);
+        //        Instantiate(go, go.transform);
+        //    }
+
+
+
+
+
+
+
+
+
+        /*int max = 100;
+
+        for (int x = 0; x < max; x++)
         {
-            for (int x = 0; x < 10; x++)
+            for (int z = 0; z < max; z++)
             {
-                    go.transform.position = new Vector3(x, 0, 0);
-                    go.transform.rotation = new Quaternion(0, 0, Random.Range(-1, 1), 1);
-                    Instantiate(go, go.transform);
+                go.transform.position = new Vector3(x, Random.Range(-1, 1), z);
+                go.transform.rotation = new Quaternion(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1), 1);
+                Instantiate(go, go.transform);
             }
+        }*/
+        //}
+    }
 
-
-
-
-
-
-
-
-
-            /*int max = 100;
-
-            for (int x = 0; x < max; x++)
+    void Update()
+    {
+        /*if (go != null)
+        {
+            if (x < 100 && z < 5)
             {
-                for (int z = 0; z < max; z++)
+
+
+
+                z_angle = Random.Range(-5, 5);
+                float z_angle_rad = z_angle * Mathf.PI / 180;
+                x_len = 1 / Mathf.Cos(z_angle_rad);
+                y_pos = (x_len * Mathf.Sin(z_angle_rad)) / 0.2f;
+
+                //if (z < 100)
+                //{
+                //    z++;
+                //}
+                //else
+                //{
+                //    x++;
+                //}
+                //previous_object_transform
+                
+                go.transform.rotation = Quaternion.Euler(0, 0, z_angle);
+                float prev_y = previous_object_transform[x == 0 ? 0 : x - 1, z] == null ? 0 : (previous_object_transform[x == 0 ? 0 : x - 1, z].position.y);
+                go.transform.position = new Vector3(x * 10, y_pos + last_y_pos, z * 10);
+                go.transform.localScale = new Vector3(x_len, 1, 1);
+
+                Instantiate(go);//, go.transform);
+                last_y_pos += y_pos * 2;
+                previous_object_transform[x, z] = go.transform;
+
+
+                if (x == 99)
                 {
-                    go.transform.position = new Vector3(x, Random.Range(-1, 1), z);
-                    go.transform.rotation = new Quaternion(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1), 1);
-                    Instantiate(go, go.transform);
+                    x = 0;*
+                    z++;
+                    last_y_pos = 0;
                 }
-            }*/
-        }
+                else
+                    x++;
+            }
+        }*/
     }
 }
---
+++ exercises/worms/Assets/Scripts/Map.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject go;
+    public Material mat;
+
+    private float x = -8.85f, y = -4.95f;
+
+    //8.85 4.95
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*for (int xx = -495; xx < 300; xx++)
+        {
+            for (int yy = -495; yy < 300; yy++)
+            {
+                x = (float)xx / 100;
+                y = (float)yy / 100;
+                go.transform.position = new Vector3(x, y, 0);
+                Instantiate(go);
+            }
+        }*/
+
+
+        go.AddComponent<MeshFilter>();
+        go.AddComponent<MeshRenderer>();
+        go.GetComponent<Renderer>().material = mat;
+
+        Mesh m = new Mesh();
+
+        Vector3[] vertices = new Vector3[160]; // создаём массив вершин
+        Vector2[] UVs = new Vector2[160]; // uv координат
+        Vector4[] tangs = new Vector4[160]; // массив касательных
+
+        int index = 0;
+        int height = 4;
+        int width = 4;
+
+        for (int x = 0; x < height; x++)
+        {
+            for (int y = 0; y < width; y++)
+            {
+                index = (int)(y * width + x);
+
+                vertices[index] = new Vector3(x, y, 0);
+                UVs[index] = new Vector2(x, y);
+            }
+        }
+        m.vertices = vertices;
+        m.uv = UVs;
+
+        index = 0;
+        int[] triangles = new int[(height-1) * (width-1) * 6 * 10];
+        for (int x = 0; x < height-1; x++)
+        {
+            for (int y = 0; y < width-1; y++)
+            {
+                //if (x != 1 || y != 1)
+                {
+                    triangles[index++] = (y * width) + x;
+                    triangles[index++] = ((y + 1) * width) + x;
+                    triangles[index++] = (y * width) + x + 1;
+
+                    triangles[index++] = ((y + 1) * width) + x;
+                    triangles[index++] = ((y + 1) * width) + x + 1;
+                    triangles[index++] = (y * width) + x + 1;
+                }
+            }
+        }
+        m.triangles = triangles;
+
+        m.RecalculateNormals();
+
+        go.GetComponent<MeshFilter>().sharedMesh = m;
+
+        //Mesh m = go.GetComponent<MeshFilter>().mesh;
+        /*int[] triangles = m.triangles;
+        triangles[0] = 0;
+        triangles[1] = 0;
+        triangles[2] = 0;
+        m.triangles = triangles;*/
+
+
+
+        /*for (int i = 0; i < m.triangles.Length; i++)
+        {
+            Debug.Log(i + " " + m.triangles[i]);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Mesh m = go.GetComponent<Mesh>();
+            //Debug.Log(go.GetComponent<MeshFilter>().sharedMesh.vertices[0]);
+            /*Debug.Log(go.GetComponent<MeshFilter>().sharedMesh.vertices[0]);
+            go.GetComponent<MeshFilter>().sharedMesh.vertices[0] = new Vector3(4.5f, 0f, 4.5f);
+            Debug.Log(go.GetComponent<MeshFilter>().sharedMesh.vertices[0]);*/
+            //Mesh m = go.GetComponent<MeshFilter>().sharedMesh;
+            /*for (int i = 0; i < m.vertices.Length; i++)
+            {
+
+                m.vertices[i] += new Vector3(0.1f, 0, 0.1f);
+
+            }*/
+
+            //m.vertices[0].Set(4f, 0f, 4f);
+
+            //go.GetComponent<MeshFilter>().sharedMesh = m;
+
+            //Debug.Log(m.vertices[0]);
+
+
+
+            //go.GetComponent<MeshFilter>().sharedMesh.vertices[0].Set(4f, 0f, 4f);
+            //go.GetComponent<MeshFilter>().sharedMesh.RecalculateBounds();
+            //Debug.Log(go.GetComponent<MeshFilter>().sharedMesh.vertices[0]);
+
+            //Mesh m = go.GetComponent<MeshFilter>().mesh;
+            //Vector3[] vertices = m.vertices;
+            /*for (int i = 0; i < m.vertices.Length; i++)
+            {
+                vertices[i] -= new Vector3(0.1f, 0, 0.1f);
+            }*/
+            /* Debug.Log(vertices[5]);
+             Debug.Log(vertices[16]);
+             Debug.Log(vertices[27]);*/
+            /*vertices[0] += new Vector3(0f, 0.1f, 0f);
+            vertices[1] += new Vector3(0f, 0.2f, 0f);
+            vertices[2] += new Vector3(0f, 0.3f, 0f);
+            vertices[3] += new Vector3(0f, 0.4f, 0f);
+            vertices[4] += new Vector3(0f, 0.5f, 0f);
+            vertices[5] += new Vector3(0f, 0.4f, 0f);
+            vertices[6] += new Vector3(0f, 0.3f, 0f);
+            vertices[7] += new Vector3(0f, 0.2f, 0f);
+            vertices[8] += new Vector3(0f, 0.1f, 0f);*/
+            //m.vertices = vertices;
+
+            //m.RecalculateNormals();
+
+            //m.RecalculateBounds();
+
+
+
+            
+        }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+        //if (x < 8.9)
+        //{
+        //    /*if (y < 3)
+        //    {
+        //        y += 0.01f;
+        //    }
+        //    else
+        //    {
+        //        y = -4.95f;
+        //        x += 0.01f;
+        //    }*/
+            //    for (int yy = -495; yy < 300; yy++)
+            //    {
+            //        y = (float)yy / 100;
+            //        go.transform.position = new Vector3(x, y, 0);
+            //        Instantiate(go);
+            //    }
+            //    x += 0.01f;
+            //}
+        }
+    }
